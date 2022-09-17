505bdf75fddda625c9abba2ac991de9737720440
habibeshadow <habibe@kayfo.games>
2022-09-17 12:00:33 +0000

    - Playing sound

--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -14,6 +14,7 @@ import {
   useTheme,
   themeColor,
 } from "react-native-rapi-ui";
+import { Audio } from 'expo-av';
 
 import {Center, Heading, ScrollView} from "native-base";
 
@@ -31,8 +32,11 @@ import * as mainReduxActions from "../store/mainRedux/mainReduxActions";
 
 const QuizScreen = (props) => {
   const { isDarkmode, setTheme } = useTheme();
+
+  const [sound, setSound] = useState(new Audio.Sound());
+  const [clickedIndex, setClickedIndex] = useState(null)
+
   const { state } = props.route.params;
-  
   const [section, setSection] = useState(state.section)
   const [quizData, setQuizData] = useState(state.quizes)
   const [sectionQuizes, setSectionQuizes] = useState([])
@@ -41,28 +45,26 @@ const QuizScreen = (props) => {
   const [quizStepper, setQuizStepper] = useState(null)
 
 
-
   const quizAnswerItem = ({ item, index }) => {
     let p = index % 2 == 0 ? "5%" : "10%";
     return <TouchableOpacity  
         key={index}
-        onPress={()=>{
-          
-          }
-        }
+        onPress={selectAnswer(index)}
 
         style={{
           elevation:3,
           width:'40%', 
           marginLeft: p, 
           marginBottom:15,
-          paddingVertical:16,
+          paddingVertical:18,
           display:"flex",
           alignItems:'center',
           borderRadius:15,
           borderColor:'gainsboro',
           borderWidth:3,
-          backgroundColor:'white',
+          backgroundColor:clickedIndex == index ? colors.primary : 'white',
+          // color:(  clickedIndex == index )? colors.primary : 'white',
+
           shadowRadius:10,
           
 
@@ -92,24 +94,28 @@ const QuizScreen = (props) => {
 
       let max = _quizes.length > 4 ? 4 : _quizes.length;
 
-      let quizItems = []
+      let quiz = {items:[], question : null}
 
+      // items
       for (let i = 0; i < max; i++) {
         
         let r = Math.floor(Math.random() * _quizes.length);
 
         const el = _quizes[r];
 
-
-        if(quizItems.indexOf(el) == -1)
+        if(quiz.items.indexOf(el) == -1)
         {
-          quizItems.push(el);
+          quiz.items.push(el);
           _quizes.splice(r,1);
         }
       
       }
+      // Sound
+      var r = Math.floor(Math.random() * quiz.items.length);
+      const soundEl = quiz.items[r];
+      quiz.question = soundEl;
 
-      _finalQuizes.push(quizItems);
+      _finalQuizes.push(quiz);
       
     }
  
@@ -123,14 +129,14 @@ const QuizScreen = (props) => {
         <ProgressStep label=""  previousBtnDisabled={true} previousBtnText="" nextBtnText="Suivant" finishBtnText="Terminer" nextBtnTextStyle={{color:colors.secondary}} onSubmit={clearTest}>
             <View style={{ alignItems: 'center', flexDirection:'column', justifyContent:'space-around'}} key="0" >
                   <FlatList
-                    data={quiz}
+                    data={quiz.items}
                     numColumns={2}
                     renderItem={quizAnswerItem}
                     keyExtractor={(item) => item.id}
                   />
               
                 <Text style={{marginTop:35}}>Appuyez pour écouter</Text>
-                <TouchableOpacity style={{ alignItems: 'center', justifyContent:'space-around', marginTop:10}} key="3">
+                <TouchableOpacity onPress={()=>playSound(quiz.question.sound)} style={{ alignItems: 'center', justifyContent:'space-around', marginTop:10}} key="3">
                   <Image source={require('../../assets/iqra-icons/play_icon.png')} style={{width:65, height:65 ,resizeMode:"center"}} />
                 </TouchableOpacity>
               
@@ -143,9 +149,15 @@ const QuizScreen = (props) => {
 
   }
 
+  
+  const selectAnswer = (index) =>{
+
+    setClickedIndex(index);
+
+  }
+
 
   const clearTest = () =>{
-
 
     let nextSection = props.mainReduxState.currentLettersSection + 1;
     if(nextSection > 5) nextSection = 5;
@@ -157,7 +169,16 @@ const QuizScreen = (props) => {
     }, 50);
   }
 
-   
+  
+  async function playSound(song) {
+
+    const { sound } = await Audio.Sound.createAsync(song);
+    setSound(sound);
+ 
+    await sound.setVolumeAsync(1); 
+    await sound.playAsync(); 
+  }
+
 
   useEffect(()=>{
 
